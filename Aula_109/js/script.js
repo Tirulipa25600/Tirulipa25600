bdb6b514d655d9885838a140c27240e8dfbed578
Tirulipa25600 <daiane256000@gmail.com>
2025-09-01 22:25:21 -0300

    Add files via upload

---
+++ Aula_109/js/script.js
@@ -0,0 +1,17 @@
+var objpf = {
+    nome: "Daiane",
+    idade: 17,
+    profissão: "programadora",
+    estatrabalhando: false,
+};
+
+// Console.log
+console.log(objpf);
+
+//Acessando um objeto
+console.log(objpf, nome);
+console.log(objpf, idade);
+
+// usando a propriedade do obj
+objpf.nome = "Teste";
+console.log(objpf.nome);
